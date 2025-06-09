576c8169b19be36d937d5f4191f4489e6e691c38
tuhinmiah202 <tuhinmiah2004@gmail.com>
2025-06-09 15:39:55 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,17 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
+  preview: {
+    allowedHosts: ['movieshubbd.onrender.com'], // ✅ This is the required fix
+  },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
